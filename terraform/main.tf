1a6601623da5462d8819153df5d56408e5433381
Ajay Neeli <ajayneeli6464@gmail.com>
2025-11-30 20:49:18 +0530

    k

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,9 @@ resource "local_file" "folder_marker" {
   content  = "This folder was created by Terraform"
   filename = "${path.cwd}/my_terraform_folder/.terraform_marker2"
 }
+
+resource "null_resource" "run_extract_script" {
+  provisioner "local-exec" {
+    command = "python3 /opt/airflow/project_root/extract.py"
+  }
+}
