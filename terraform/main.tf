d2ffe930d21af22927bd1aeae7410d90134c93e0
Ajay Neeli <ajayneeli6464@gmail.com>
2025-11-30 12:57:57 +0530

    update

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+}
+
+# Create a folder on Windows
+resource "local_file" "folder_marker" {
+  content  = "This folder was created by Terraform"
+  filename = "${path.cwd}/my_terraform_folder/.terraform_marker3"
+}
